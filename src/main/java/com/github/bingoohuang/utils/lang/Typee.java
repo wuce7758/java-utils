596666cf4e4eac52c8721e724c5883a2ff611392
bingoohuang <bingoo.huang@gmail.com>
2015-09-07 14:53:49 +0800

    typee refactor

--- src/main/java/com/github/bingoohuang/utils/lang/Typee.java
+++ src/main/java/com/github/bingoohuang/utils/lang/Typee.java
@@ -5,15 +5,33 @@ import java.lang.reflect.Type;
 
 public class Typee {
 
-    public static Class<?> getActualTypeArgument(Class<?> clz, int order) {
-        Type genericType = clz.getGenericInterfaces()[order];
-        if (!ParameterizedType.class.isAssignableFrom(genericType.getClass())) return Void.class;
+    public static Class<?> getActualTypeArgument(
+            Class<?> subClass,
+            Class<?> genericInterface
+    ) {
+        return getActualTypeArgument(subClass, genericInterface, 0);
+    }
 
-        ParameterizedType parameterizedType = (ParameterizedType) genericType;
-        try {
-            return (Class<?>) parameterizedType.getActualTypeArguments()[0];
-        } catch (ClassCastException ex) {
-            return Void.class;
+    public static Class<?> getActualTypeArgument(
+            Class<?> subClass,
+            Class<?> genericInterface,
+            int argumentOrder
+    ) {
+        for (Type generic : subClass.getGenericInterfaces()) {
+            if (!(generic instanceof ParameterizedType)) continue;
+
+            ParameterizedType pt = (ParameterizedType) generic;
+            if (pt.getRawType() != genericInterface) continue;
+
+            Type type = pt.getActualTypeArguments()[argumentOrder];
+            return (Class<?>) type;
         }
+
+        Class<?> superClz = subClass.getSuperclass();
+        if (superClz == Object.class) return null;
+
+        return getActualTypeArgument(superClz,
+                genericInterface,
+                argumentOrder);
     }
 }
